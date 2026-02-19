6fdefca90aea9168e7ad320ea03e906b4abc74e5
Anvesha Kumar <anv90@users.noreply.github.com>
2026-02-19 13:23:20 -0800

    set up basic get route for quotes

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.get('/quotes/<num_quotes>')
+def get_quotes():
+    """ GET request from client requesting quotes
+        Return number of quotes to client 
+    """
+    quote_list = generate_quotes(num_quotes) #implement func
+
+    return jsonify(quote_list), 200
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=1400, debug=True)
