ad58b017e38a061c5beab3ed10bf8355106ce457
Anvesha Kumar <kumaranv@oregonstate.edu>
2026-02-19 13:34:07 -0800

    Add function to generate random quotes
    Added a function to generate random quotes from quotes.txt.

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+def generate_quotes(num):
+    """select num of quotes randomly from quotes.txt""" 
+    return 0
+
 @app.get('/quotes/<num_quotes>')
 def get_quotes():
     """ GET request from client requesting quotes
